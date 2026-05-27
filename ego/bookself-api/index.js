6e00abcaaaaaeec1be1ae6b9fd71dd05963d3441
Aayush-Mistri <aayushmistri810@gmail.com>
2026-05-27 16:02:46 +0530

    added another project bookself api

---
+++ ego/bookself-api/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+
+let books = []
+
+app.get('/', (req, res) => {
+    res.json({ message: "Bookshelf API working" })
+})
+
+app.get('/books', (req, res) => {
+    res.json(books)
+})
+
+app.post('/books', (req, res) => {
+    const book = req.body;
+    book.id = Date.now()
+    books.push(book);
+    res.json({ message: "Book added", book })
+})
+
+app.listen(3000, () => {
+    console.log("Server running on port 3000")
+})
