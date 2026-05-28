739ec26c18a0e490024873e4f72470a97dd30396
Aayush-Mistri <aayushmistri810@gmail.com>
2026-05-28 11:30:16 +0530

    feature added

--- ego/bookself-api/index.js
+++ ego/bookself-api/index.js
@@ -21,6 +21,8 @@ app.post('/books', (req, res) => {
     res.json({ message: "Book added", book })
 })
 
+
+// the server is listening on port 3000
 app.listen(3000, () => {
     console.log("Server running on port 3000")
 })
