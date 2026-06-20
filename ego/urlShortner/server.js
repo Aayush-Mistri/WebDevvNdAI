66d37eaf3c6d57e2e1928aadae460d7326c21663
Aayush Mistri <aayushhmistri@gmail.com>
2026-06-20 14:53:34 +0530

    url shortner added

--- ego/urlShortner/server.js
+++ ego/urlShortner/server.js
@@ -15,6 +15,7 @@ app.post('/shorten', (req, res) => {
 
   const code = crypto.randomBytes(4).toString('hex');
   store[code] = url;
+  console.log(code)
 
   res.json({
     shortCode: code,
@@ -22,9 +23,14 @@ app.post('/shorten', (req, res) => {
   });
 });
 
+
 app.get('/:code', (req, res) => {
-  const { code } = req.params;
+  const code  = req.params.code;
   const url = store[code];
+
+  console.log(code)
+  console.log(url)
+  console.log(store)
 
   if (!url) {
     return res.status(404).json({ error: 'URL not found' });
